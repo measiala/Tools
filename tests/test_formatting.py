1f84f080271c5b91388964055ce1658ef7a1f24d
Mark E Asiala <13490097+measiala@users.noreply.github.com>
2021-09-12 22:11:27 -0400

    Working through coverage of test_method_helpers

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -2,18 +2,18 @@
 import pytest
 
 # Import global modules
-import collections
 import dataclasses
 import pathlib
-import sys
 import typing
 
 # Import module to test
 from ..formatting import \
-    fmt_bool, fmt_float, fmt_int, fmt_none, fmt_str, fmt_dict, fmt_list, fmt_set, fmt_tuple, \
+    fmt_bool, fmt_float, fmt_int, fmt_none, fmt_str, \
+    fmt_dict, fmt_list, fmt_set, fmt_tuple, \
     fmt_value, fmt_dataclass, val2txt, txt2val, str2list, \
     process_container, get_ga_types, get_dc_type_hints, populate_list, \
     define_dataclass, read_txt, write_txt, write_txt_class, write_txt_row
+
 
 def test_get_ga_types():
     assert get_ga_types(typing.List[str]) == (list, (str,))
@@ -21,6 +21,7 @@ def test_get_ga_types():
     assert get_ga_types(typing.List[T]) == (list, (T,))
     with pytest.raises(TypeError):
         get_ga_types(list)
+
 
 def test_get_dc_type_hints():
     @dataclasses.dataclass
@@ -51,8 +52,9 @@ def test_get_dc_type_hints():
         'e': list
     }
 
+
 def test_fmt_bool():
-    assert fmt_bool(True, bool) == True
+    assert fmt_bool(True, bool) is True
     assert fmt_bool(True, int) == 1
     assert fmt_bool(False, int) == 0
     assert fmt_bool(True, str) == 'True'
@@ -66,6 +68,7 @@ def test_fmt_bool():
         fmt_bool(True, float)
     with pytest.raises(ValueError):
         fmt_bool(True, dict)
+
 
 def test_fmt_float():
     assert fmt_float(1.5, float) == 1.5
@@ -82,12 +85,13 @@ def test_fmt_float():
     with pytest.raises(ValueError):
         fmt_float(1.5, int)
 
+
 def test_fmt_int():
     assert fmt_int(37, int) == 37
-    assert fmt_int(37, bool) == True
-    assert fmt_int(-37, bool) == True
-    assert fmt_int(0, bool) == False
-    assert fmt_int(1, bool) == True
+    assert fmt_int(37, bool) is True
+    assert fmt_int(-37, bool) is True
+    assert fmt_int(0, bool) is False
+    assert fmt_int(1, bool) is True
     assert fmt_int(37, str) == '37'
     assert fmt_int(37, float) == 37.0
     assert fmt_int(37, list) == [37]
@@ -99,8 +103,9 @@ def test_fmt_int():
     with pytest.raises(ValueError):
         fmt_int(37, dict)
 
+
 def test_fmt_none():
-    assert fmt_none(None, None) == None
+    assert fmt_none(None, None) is None
     with pytest.raises(ValueError):
         fmt_none(None, bool)
     with pytest.raises(ValueError):
@@ -116,14 +121,15 @@ def test_fmt_none():
     assert fmt_none(None, typing.Set[str]) == set()
     assert fmt_none(None, typing.Tuple[str]) == tuple()
 
+
 def test_fmt_str():
     assert fmt_str('123', str) == '123'
     assert fmt_str('123', list) == ['123']
     assert fmt_str('123', set) == {'123'}
     assert fmt_str('123', tuple) == ('123',)
-    assert fmt_str('123', bool) == False
-    assert fmt_str('True', bool) == True
-    assert fmt_str('False', bool) == False
+    assert fmt_str('123', bool) is False
+    assert fmt_str('True', bool) is True
+    assert fmt_str('False', bool) is False
     assert fmt_str('foo.bar', pathlib.PosixPath) == pathlib.PosixPath('foo.bar')
     assert fmt_str('foo.bar', pathlib.Path) == pathlib.Path('foo.bar')
     assert fmt_str('', str) == ''
@@ -145,6 +151,7 @@ def test_fmt_str():
     assert fmt_str('', typing.Set[int]) == set()
     assert fmt_str('', typing.Tuple[int]) == tuple()
 
+
 def test_fmt_dict():
     assert fmt_dict({}, dict) == {}
     assert fmt_dict({1: '1'}, dict) == {1: '1'}
@@ -155,6 +162,7 @@ def test_fmt_dict():
     with pytest.raises(ValueError):
         fmt_dict({1: '1'}, bool)
 
+
 def test_fmt_list():
     assert fmt_list([], list) == []
     assert fmt_list([1], list) == [1]
@@ -162,7 +170,7 @@ def test_fmt_list():
     assert fmt_list([1, 2, 3], tuple) == (1, 2, 3)
     assert fmt_list([], str) == ''
     assert fmt_list([1, 2, 3], str) == '1,2,3'
-    assert fmt_list([1, 2, 3, 4], dict) == { 1: 2, 3: 4}
+    assert fmt_list([1, 2, 3, 4], dict) == {1: 2, 3: 4}
     with pytest.raises(ValueError):
         fmt_list([1, 2, 3], dict)
     assert fmt_list([1, '1', 2, '2'], typing.List[str]) == ['1', '1', '2', '2']
@@ -172,6 +180,7 @@ def test_fmt_list():
     assert fmt_list([1, '1', 2, '2'], typing.Set[str]) == {'1', '2'}
     assert fmt_list([1, '1', 2, '2'], typing.Tuple[int]) == (1, 1, 2, 2)
     assert fmt_list([], typing.Set[str]) == set()
+
 
 def test_fmt_set():
     assert fmt_set(set(), set) == set()
@@ -189,12 +198,13 @@ def test_fmt_set():
     with pytest.raises(ValueError):
         fmt_set({1, 2}, bool)
 
+
 def test_fmt_tuple():
     assert fmt_tuple((), set) == set()
     assert fmt_tuple((), list) == []
     assert fmt_tuple((), tuple) == ()
-    assert fmt_tuple((1, '2'), tuple) == (1, '2') # Volatile going from unordered to ordered
-    assert fmt_tuple((1, '2'), list) == [1, '2']  # Volatile going from unordered to ordered
+    assert fmt_tuple((1, '2'), tuple) == (1, '2')   # Volatile going from unordered to ordered
+    assert fmt_tuple((1, '2'), list) == [1, '2']    # Volatile going from unordered to ordered
     assert fmt_tuple((1, '2'), set) == {1, '2'}
     assert fmt_tuple((1, '2'), str) == '1,2'
     assert fmt_tuple((1, '1', 2, '2'), typing.List[str]) == ['1', '1', '2', '2']
@@ -205,15 +215,16 @@ def test_fmt_tuple():
     with pytest.raises(ValueError):
         fmt_tuple((1, 2), bool)
 
+
 def test_fmt_value():
     # String
     assert fmt_value('123', str) == '123'
     assert fmt_value('123', list) == ['123']
     assert fmt_value('123', set) == {'123'}
     assert fmt_value('123', tuple) == ('123',)
-    assert fmt_value('123', bool) == False
-    assert fmt_value('True', bool) == True
-    assert fmt_value('False', bool) == False
+    assert fmt_value('123', bool) is False
+    assert fmt_value('True', bool) is True
+    assert fmt_value('False', bool) is False
     assert fmt_value('foo.bar', pathlib.PosixPath) == pathlib.PosixPath('foo.bar')
     assert fmt_value('foo.bar', pathlib.Path) == pathlib.Path('foo.bar')
     assert fmt_value('', str) == ''
@@ -228,11 +239,11 @@ def test_fmt_value():
     assert fmt_value([1, 2, 3], set) == {1, 2, 3}
     assert fmt_value([1, 2, 3], tuple) == (1, 2, 3)
     assert fmt_value([1, 2, 3], str) == '1,2,3'
-    assert fmt_value([1, 2, 3, 4], dict) == { 1: 2, 3: 4}
+    assert fmt_value([1, 2, 3, 4], dict) == {1: 2, 3: 4}
     with pytest.raises(ValueError):
         fmt_value([1, 2, 3], dict)
     # Boolean
-    assert fmt_value(True, bool) == True
+    assert fmt_value(True, bool) is True
     assert fmt_value(True, int) == 1
     assert fmt_value(True, str) == 'True'
     assert fmt_value(True, list) == [True]
@@ -244,9 +255,9 @@ def test_fmt_value():
         fmt_value(True, dict)
     # Integer
     assert fmt_value(37, int) == 37
-    assert fmt_value(37, bool) == True
-    assert fmt_value(0, bool) == False
-    assert fmt_value(1, bool) == True
+    assert fmt_value(37, bool) is True
+    assert fmt_value(0, bool) is False
+    assert fmt_value(1, bool) is True
     assert fmt_value(37, str) == '37'
     assert fmt_value(37, float) == 37.0
     assert fmt_value(37, list) == [37]
@@ -261,6 +272,7 @@ def test_fmt_value():
     assert fmt_value({1, '2'}, str) == '1,2'
     with pytest.raises(ValueError):
         fmt_value(pathlib.Path('./'), str)
+
 
 def test_fmt_dataclass():
     # Set up
@@ -277,7 +289,7 @@ def test_fmt_dataclass():
     # Verify result of txt2val, all list-like things have been cast as a list
     dc_val = [True, 2, 4, 'foo.bar', ['foo', 'bar'], ['foo', 'bar'], ['1', '2']]
     dc = Test(*dc_val)
-    assert dc.b == True
+    assert dc.b is True
     assert dc.f == 2
     assert dc.n == 4
     assert dc.s == 'foo.bar'
@@ -286,7 +298,7 @@ def test_fmt_dataclass():
     assert dc.si == ['1', '2']
 
     dcf = fmt_dataclass(dc)
-    assert dcf.b == True
+    assert dcf.b is True
     assert dcf.f == 2.0
     assert dcf.n == 4
     assert dcf.s == 'foo.bar'
@@ -306,9 +318,10 @@ def test_fmt_dataclass():
     with pytest.raises(ValueError):
         fmt_dataclass(dc)
 
-##
-## Routines to convert a value from one type to text and vice versa
-##
+#
+# Routines to convert a value from one type to text and vice versa
+#
+
 
 def test_str2list():
     assert str2list('12') == ['12']
@@ -318,6 +331,7 @@ def test_str2list():
     with pytest.raises(ValueError):
         str2list(1)
 
+
 def test_val2txt():
     assert val2txt(None) == ''
     assert val2txt(1) == '1'
@@ -325,22 +339,23 @@ def test_val2txt():
     assert val2txt(1.50) == '1.5'
     assert val2txt(True) == 'True'
     assert val2txt({1}) == '1'
-    assert val2txt({1,2}) == '1,2'
+    assert val2txt({1, 2}) == '1,2'
     assert val2txt([1]) == '1'
-    assert val2txt([1,2]) == '1,2'
+    assert val2txt([1, 2]) == '1,2'
     assert val2txt((1,)) == '1'
-    assert val2txt((1,2)) == '1,2'
+    assert val2txt((1, 2)) == '1,2'
     assert val2txt({'1': 's'}) == '1,s'
-    assert val2txt([[1,2],3]) == '1,2,3'
-    assert val2txt([1,'s',True]) == '1,s,True'
+    assert val2txt([[1, 2], 3]) == '1,2,3'
+    assert val2txt([1, 's', True]) == '1,s,True'
     assert val2txt(pathlib.PosixPath('./foo/bar.foo')) == 'foo/bar.foo'
     with pytest.raises(ValueError):
         val2txt(ValueError)
 
+
 def test_txt2val():
-    assert txt2val(None) == None
+    assert txt2val(None) is None
     assert txt2val(1) == 1
-    assert txt2val('None') == None
+    assert txt2val('None') is None
     assert txt2val('1') == 1
     assert txt2val('1.0') == 1
     assert txt2val('1.5') == 1.5
@@ -352,12 +367,13 @@ def test_txt2val():
     assert txt2val('0o770x') == '0o770x'
     assert txt2val('0x110') == 1*256 + 1*16
     assert txt2val('0x110x') == '0x110x'
-    assert txt2val('True') == True
-    assert txt2val('False') == False
-    assert txt2val('true') == True
-    assert txt2val('1,2') == [1,2]
+    assert txt2val('True') is True
+    assert txt2val('False') is False
+    assert txt2val('true') is True
+    assert txt2val('1,2') == [1, 2]
     assert txt2val('a,1') == ['a', 1]
     assert txt2val('1,s,True') == [1, 's', True]
+
 
 def test_process_container():
     # Set up
@@ -365,6 +381,7 @@ def test_process_container():
     class TestDC:
         x: int
         y: set
+
     class FailTest:
         def __init__(self, b, f):
             self.b = b
@@ -381,25 +398,26 @@ def test_process_container():
     with pytest.raises(ValueError):
         process_container({'1', '2'}, str)
     # Container is list of parameters for one dataclass
-    assert process_container([1, {1,2}], TestDC) == [TestDC(x=1, y={1,2})]
+    assert process_container([1, {1, 2}], TestDC) == [TestDC(x=1, y={1, 2})]
     # Container is a list of list of parameters to define a list of dataclasses
-    assert process_container([[1, {1,2}], [2, {3,4}]], TestDC) == [TestDC(x=1, y={1,2}), TestDC(2, {3,4})]
+    assert process_container([[1, {1, 2}], [2, {3, 4}]], TestDC) == [TestDC(x=1, y={1, 2}), TestDC(2, {3, 4})]
     # Container is dataclass
-    assert process_container(TestDC(1, {1,2}), TestDC) == [TestDC(x=1, y={1,2})]
+    assert process_container(TestDC(1, {1, 2}), TestDC) == [TestDC(x=1, y={1, 2})]
     # Container is a list of dataclasses
-    assert process_container([TestDC(1, {1,2}), TestDC(2, {3,4})], TestDC) == [TestDC(x=1, y={1,2}), TestDC(2, {3,4})]
+    assert process_container([TestDC(1, {1, 2}), TestDC(2, {3, 4})], TestDC) == [TestDC(x=1, y={1, 2}), TestDC(2, {3, 4})]
     with pytest.raises(ValueError):
-        process_container([TestDC(1, {1,2}), [1, {1,2}]], TestDC)
+        process_container([TestDC(1, {1, 2}), [1, {1, 2}]], TestDC)
     with pytest.raises(TypeError):
-        process_container([TestDC(1, {1,2}), [1, {1,2}]], FailTest)
+        process_container([TestDC(1, {1, 2}), [1, {1, 2}]], FailTest)
 
-##
-## Helpers to read and write dataclasses to lists and vice versa
-##
+#
+# Helpers to read and write dataclasses to lists and vice versa
+#
+
 
 def test_populate_list():
     class TestClass:
-        def __init__(self, name, groups, platforms, other = None):
+        def __init__(self, name, groups, platforms, other=None):
             self.name = name
             self.groups = groups
             self.platforms = platforms
@@ -425,9 +443,10 @@ def test_populate_list():
                 TestClass('nemo', ['g1', 'g2'], ['p1', 'p2'], 'thing'), DtClass2
             ) == [['g1', 'g2'], 'nemo', ['p1', 'p2'], '']
 
+
 def test_define_dataclass():
     class TestClass:
-        def __init__(self, name, groups, platforms, other = None):
+        def __init__(self, name, groups, platforms, other=None):
             self.name = name
             self.groups = groups
             self.platforms = platforms
@@ -449,15 +468,17 @@ def test_define_dataclass():
                 TestClass('nemo', ['g1', 'g2'], ['p1', 'p2'], 'thing'), TestClass
             )
 
+
 def test_read_txt():
     t_str = '1,2|s,t|True'
     t_list = t_str.split('|')
     assert read_txt(t_list) == [[1, 2], ['s', 't'], True]
 
+
 def test_write_txt_class():
     # Set up
     class TestClass:
-        def __init__(self, name, groups, platforms, other = None):
+        def __init__(self, name, groups, platforms, other=None):
             self.name = name
             self.groups = groups
             self.platforms = platforms
@@ -480,7 +501,7 @@ def test_write_txt_class():
 def test_write_txt():
     # Set up
     class TestClass:
-        def __init__(self, name, groups, platforms, other = None):
+        def __init__(self, name, groups, platforms, other=None):
             self.name = name
             self.groups = groups
             self.platforms = platforms
@@ -497,9 +518,9 @@ def test_write_txt():
 
 
 def test_write_txt_row():
-    ## assert write_txt_row()
+    # assert write_txt_row()
     class TestClass:
-        def __init__(self, name, groups, platforms, other = None):
+        def __init__(self, name, groups, platforms, other=None):
             self.name = name
             self.groups = groups
             self.platforms = platforms
--- tests/test_method_helpers.py
+++ tests/test_method_helpers.py
@@ -25,10 +25,15 @@ class Type2Class:
     field2: str
 
 @dataclasses.dataclass
+class Type3Class:
+    field1: str
+
+@dataclasses.dataclass
 class UmbrellaClass:
     name: str
     t1dict: dict = dataclasses.field(init=False, default_factory=dict)
     t2dict: dict = dataclasses.field(init=False, default_factory=dict)
+    t3dict: dict = dataclasses.field(init=False, default_factory=dict)
 
     def add_t1(self, t1_inst):
         self.t1dict[t1_inst.var1] = t1_inst
@@ -36,14 +41,11 @@ class UmbrellaClass:
     def add_t2(self, t2_inst):
         self.t2dict[t2_inst.field1] = t2_inst
 
+    def add_t3(self, t3_inst):
+        self.t3dict[t3_inst.field1] = t3_inst
+
 # All of these tests are inherently tested by the various classes that call them
 # but we should add in separate test files and unit tests
-
-def test_base_read_file():
-    pass
-
-def test_base_write_file():
-    pass
 
 def test_base_read_xls():
     uc = UmbrellaClass('Test XLS')
@@ -68,6 +70,46 @@ def test_base_read_xls():
         'Value1.4': Type2Class(field1='Value1.4', field2='Value2.4')
     }
 
+    # Create various errors
+    uc1 = UmbrellaClass('Error Instance')
+
+    # Incorrect number of list items
+    with pytest.raises(ValueError):
+        base_read_xls(
+            root / 'tests' / 'test_xls.xls',
+            [
+                ['1epyT', uc.add_t1]
+            ]
+        )
+    
+    # Sheet name not found
+    with pytest.raises(ValueError):
+        base_read_xls(
+            root / 'tests' / 'test_xls.xls',
+            [
+                ['1epyT', uc.add_t1, Type1Class]
+            ]
+        )
+
+    # Record type mismatch in number of records
+    with pytest.raises(TypeError):
+        base_read_xls(
+            root / 'tests' / 'test_xls.xls',
+            [
+                ['Type1', uc.add_t3, Type3Class]
+            ]
+        )
+
+    # Record type mismatch in number of records
+    with pytest.raises(ValueError):
+        base_read_xls(
+            root / 'tests' / 'test_xls.xls',
+            [
+                ['Type1', uc.add_t3, Type1Class]
+            ]
+        )
+
+
 def test_base_read_xlsx():
     uc = UmbrellaClass('Test XLSX')
     assert base_read_xlsx(
@@ -91,6 +133,7 @@ def test_base_read_xlsx():
         'Value1.4': Type2Class(field1='Value1.4', field2='Value2.4')
     }
 
+
 def test_base_write_file():
     uc = UmbrellaClass('Test XLSX')
     assert base_read_xlsx(
@@ -100,14 +143,46 @@ def test_base_write_file():
             ['Type2', uc.add_t2, Type2Class]
         ]
     ) == (9, [5, 4])
+
+    # Write out one record with no record type prefix to .._1
     assert base_write_file(
-        './tests/test_asc.txt',
+        root / 'tests' / 'test_asc_1.txt',
+        [
+            [None, uc.t1dict, Type1Class]
+        ]
+    ) == 5
+
+    # Verify contents of .._1
+    with open(root / 'tests' / 'test_asc_1.txt', 'r') as infile:
+        f = infile.read()
+    assert f == \
+        'Value1.1|Value2.1\n' \
+        + 'Value1.2|Value2.2\n' \
+        + 'Value1.3|Value2.3\n' \
+        + 'Value1.4|Value2.4\n' \
+        + 'Value1.5|Value2.5\n'
+    
+    # Trigger ValueError if rt_lists is incomplete
+    with pytest.raises(ValueError):
+        assert base_write_file(
+            root / 'tests' / 'test_asc_2.txt',
+            [
+                ['Type1', uc.t1dict, Type1Class],
+                ['Type2', uc.t2dict]
+            ]
+        )
+
+    # Standard final write to .._2
+    assert base_write_file(
+        root / 'tests' / 'test_asc_2.txt',
         [
             ['Type1', uc.t1dict, Type1Class],
             ['Type2', uc.t2dict, Type2Class]
         ]
-    )
-    with open('./tests/test_asc.txt', 'r') as infile:
+    ) == 9
+
+    # Verify contents of .._2
+    with open(root / 'tests' / 'test_asc_2.txt', 'r') as infile:
         f = infile.read()
     assert f == \
         'Type1|Value1.1|Value2.1\n' \
@@ -120,7 +195,9 @@ def test_base_write_file():
         + 'Type2|Value1.3|Value2.3\n' \
         + 'Type2|Value1.4|Value2.4\n'
 
+
 def test_base_read_file():
+    # Read in base line
     uc0 = UmbrellaClass('Test XLSX')
     assert base_read_xlsx(
         root / 'tests' / 'test_xlsx.xlsx',
@@ -129,15 +206,37 @@ def test_base_read_file():
             ['Type2', uc0.add_t2, Type2Class]
         ]
     ) == (9, [5, 4])
+
+    # Read in standard 2 class from .._2
     uc1 = UmbrellaClass('Test XLSX')
     assert base_read_file(
-        root / 'tests' / 'test_asc.txt',
+        root / 'tests' / 'test_asc_2.txt',
         [
             ['Type1', uc1.add_t1, Type1Class],
             ['Type2', uc1.add_t2, Type2Class]
         ]
     ) == 9
     assert uc0 == uc1
+
+    # Read in just t1 class from .._1
+    uc2 = UmbrellaClass('Test XLSX')
+    assert base_read_file(
+        root / 'tests' / 'test_asc_1.txt',
+        [
+            [None, uc2.add_t1, Type1Class]
+        ]
+    ) == 5
+    assert uc0.t1dict == uc2.t1dict
+
+    # Trigger Value Error
+    uc3 = UmbrellaClass('Test XLSX')
+    with pytest.raises(ValueError):
+        base_read_file(
+            root / 'tests' / 'test_asc_1.txt',
+            [
+                [uc2.add_t1, Type1Class]
+            ]
+        )
 
 def test_base_add_item():
     @dataclasses.dataclass
